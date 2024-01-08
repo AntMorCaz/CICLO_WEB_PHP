d2d32a8308ae7325745083ed80acef6328c73213
AntMorCaz <155962199+AntMorCaz@users.noreply.github.com>
2024-01-08 11:07:54 +0100

    Create index.php

---
+++ COSAS/index.php
@@ -0,0 +1,15 @@
+<?php
+    $_SESSION["hola"] = "adios";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
